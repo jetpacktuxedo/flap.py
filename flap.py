86df8d06f4f5d412fd01560129b4ae31e8052d11
Ethan Madden <emadden@desktop.isilon.com>
2014-07-23 16:49:32 -0700

    Made the bird an object as well, because it should make checking collisions easier later.

--- flap.py
+++ flap.py
@@ -2,7 +2,54 @@
 
 import curses, curses.textpad, time, sys
 
-maxX, maxY, height, oldheight, quit, rate = 0, 0, 0, 0, 1, 0.04
+maxX, maxY, quit = 0, 0, 1
+
+class Bird(object):
+  height = 0
+  oldheight = 0
+  rate = 0.04
+  xPos = 1
+  screen = None
+
+  def __init__(self, screen, height, xPos):
+    self.screen = screen
+    self.height = height
+    self.xPos = xPos
+
+  def setHeight(self, newheight):
+    if (abs(self.oldheight - self.height) > 1): self.oldheight = self.height
+    self.height = newheight
+
+  def setDeltaHeight(self, dHeight):
+    if (abs(self.oldheight - self.height) > 1): self.oldheight = self.height
+    self.height += dHeight
+
+  def resetAccel(self):
+    self.rate = 0.04
+
+  def gravity(self):
+    self.rate *= 1.005
+
+  def draw(self):
+    self.screen.addstr(int(round(self.height-1)), int(round(self.xPos)), " ___   ")
+    self.screen.addstr(int(round(self.height+0)), int(round(self.xPos)), "/__O\_ ")
+    self.screen.addstr(int(round(self.height+1)), int(round(self.xPos)), "\___/-'")
+
+  #Returns true if there is a collision
+  def flapFall(self, key):
+    global quit
+    if (key == 32): 
+      self.setDeltaHeight(-2)
+      self.resetAccel()
+    else: 
+      self.setDeltaHeight(self.rate)
+      self.gravity()
+    if (self.height <= 1 or self.height >= (maxY - 2)): return True
+    else: return False
+
+  def moved(self):
+    if (abs(self.oldheight - self.height) >= 1): return True
+    else: return False
 
 class Pipe(object):
   height = 0
@@ -18,7 +65,7 @@ class Pipe(object):
     if (self.dist <= 0.25 and self.thickness > 1): 
       self.thickness = self.thickness - 0.25
       self.dist = 0.25
-    elif (self.dist <= 0.5 and self.thickness <= 1):
+    elif (self.dist <= 0.25 and self.thickness <= 1):
       self.thickness = 0
       self.dist = 0
     else: self.dist = self.dist - 0.25
@@ -35,17 +82,33 @@ class Pipe(object):
     if (self.dist % 1.0 == 0): return True
     else: return False
 
+  def collision(self, birdMinX, birdMaxX, birdMinY, birdMaxY):
+    pipeMinX = dist
+    pipeMaxX = dist + thickness
+    pipeMinY = height
+    pipeMaxY = maxY - height
+    for x in range(birdMinX, birdMaxX):
+      for y in range(birdMinY, birdMaxY):
+        if (x in range(pipeMinX, pipeMaxX) and y in range(pipeMinY, pipeMaxY)):
+          return True
+    return False
+
 def checkPipesForWhole(pipeList):
   for i in pipeList:
     if i.wholePos(): return True
   return False
 
-def resetVars(screen):
+def checkCollisions(pipeList, birdMinX, birdMaxX, birdMinY, birdMaxY):
+  for i in pipeList:
+    if i.collision(birdMinX, birdMaxX, birdMinY, birdMaxY): return True
+  return False
+
+def resetVars(screen, bird):
   global maxX, maxY, quit, pipePos
   maxY, maxX = screen.getmaxyx()
   quit = 1
-  setHeight((maxY / 2) - 2)
-  resetAccel()
+  bird.setHeight((maxY / 2) - 2)
+  bird.resetAccel()
   pipePos = maxX - 15
 
 def initScreen(screen):
@@ -56,52 +119,13 @@ def initScreen(screen):
   screen.nodelay(1)
   screen.refresh()
 
-def scrprint(window, x, y, string):
-  window.addstr(y, x, string)
-  window.refresh()
-
-def setHeight(newheight):
-  global height, oldheight
-  if (abs(oldheight - height) > 1): oldheight = height
-  height = newheight
-
-def setDeltaHeight(dHeight):
-  global height, oldheight
-  if (abs(oldheight - height) > 1): oldheight = height
-  height = height + dHeight
-
-def resetAccel():
-  global rate
-  rate = 0.04
-
-def gravity():
-  global rate
-  rate = rate * 1.005
-
-def drawBird(screen, y):
-  screen.addstr(y-1, 2, " ___   ")
-  screen.addstr(y+0, 2, "/__O\_ ")
-  screen.addstr(y+1, 2, "\___/-'")
-
-def refreshScreen(screen, pipeList):
-  if (abs(oldheight - height) >= 1 or checkPipesForWhole(pipeList)):
+def refreshScreen(screen, bird, pipeList):
+  if (bird.moved() or checkPipesForWhole(pipeList)):
     screen.clear()
     screen.box()
-    drawBird(screen, int(round(height)))
+    bird.draw()
     for i in pipeList: i.draw(screen)
     screen.refresh()
-
-def flapFall(screen, key):
-  global quit
-  if (key == 32): 
-    setDeltaHeight(-2)
-    resetAccel()
-  else: 
-    setDeltaHeight(rate)
-    gravity()
-  if (height <= 1 or height >= (maxY - 2)): 
-    while (quit != 0): quit = lose()
-    resetVars(screen)
 
 def lose():
   centerY = maxY/2
@@ -119,8 +143,10 @@ def lose():
   else: return 1
 
 def main(screen):
+  global quit
   initScreen(screen)
-  resetVars(screen)
+  bird = Bird(screen, (maxY / 2) - 2, 2)
+  resetVars(screen, bird)
   c = 0
   pipeList = []
   pipeList.append(Pipe(15, maxX - 10, 10))
@@ -129,8 +155,11 @@ def main(screen):
   while (c != 113):
     time.sleep(1/60.0)
     for i in pipeList: i.advance()
-    refreshScreen(screen, pipeList)
-    flapFall(screen, c)
+    refreshScreen(screen, bird, pipeList)
+
+    if bird.flapFall(c):
+      while (quit != 0): quit = lose()
+      resetVars(screen, bird)
     c = screen.getch()
     
 try:
